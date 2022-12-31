fb377a35932e54ba348d56b905d75f3a33067741
kimsniper <kim1991mail@gmail.com>
2022-12-31 21:17:23 +0800

    Coding

--- examples/esp32_implementation/main/example_usage.c
+++ examples/esp32_implementation/main/example_usage.c
@@ -23,7 +23,7 @@ void app_main(void)
         .alarm1_en = FALSE,
         .alarm2_en = FALSE,
         .rate_select = RATE_SELECT_1HZ,
-        .clock_format = FORMAT_24HR
+        .clock_format = FORMAT_12HR
     };
 
     err = ds3231_i2c_dev_config(cfg);
@@ -33,13 +33,14 @@ void app_main(void)
     }
     else{
         ESP_LOGE(TAG, "Device config failed");
+        return;
     }
 
     ds3231_rtcc_clock_t clock_set = {
-        .hr = 19,
-        .sec = 5,
-        .min = 30,
-        .am_pm = CLOCK_PERIOD_AM
+        .hr = 11,
+        .min = 59,
+        .sec = 50,
+        .am_pm = CLOCK_PERIOD_PM
     };
 
     err += ds3231_i2c_set_clock(cfg, clock_set);
@@ -50,6 +51,7 @@ void app_main(void)
     }
     else{
         ESP_LOGE(TAG, "Time setting failed");
+        return;
     }
 
     ds3231_rtcc_calendar_t calendar_set = {
@@ -66,6 +68,7 @@ void app_main(void)
     }
     else{
         ESP_LOGE(TAG, "Calendar setting failed");
+        return;
     }
 
     if (err == DS3231_OK)
@@ -78,7 +81,18 @@ void app_main(void)
             //Reading here
             ds3231_i2c_read_calendar(&calendar_data);
             ds3231_i2c_read_clock(cfg, &clock_data);
-            ESP_LOGI(TAG, "Time: %02d:%02d:%02d", clock_data.hr, clock_data.min, clock_data.sec);
+            if(cfg.clock_format == FORMAT_24HR)
+            {
+                ESP_LOGI(TAG, "Time: %02d:%02d:%02d", clock_data.hr, clock_data.min, clock_data.sec);
+            }
+            else
+            {
+                ESP_LOGI(TAG, "Time: %02d:%02d:%02d %s", clock_data.hr, 
+                                                         clock_data.min, 
+                                                         clock_data.sec, 
+                                                         clock_data.am_pm ? "PM" : "AM"
+                                                         );
+            }
             ESP_LOGI(TAG, "Date: %02d/%02d/%02d", calendar_data.mon, calendar_data.date, calendar_data.year);
             ds3231_i2c_hal_ms_delay(1000);
         }
